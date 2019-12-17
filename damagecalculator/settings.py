f2059d26b434d27c5f4bdac86bb7ca790ec50663
trkea <trs1141@yahoo.co.jp>
2019-12-17 22:54:42 +0900

    commentout STATICFILES_STORAGE(Heroku test)

--- damagecalculator/settings.py
+++ damagecalculator/settings.py
@@ -127,7 +127,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # add
 try:
